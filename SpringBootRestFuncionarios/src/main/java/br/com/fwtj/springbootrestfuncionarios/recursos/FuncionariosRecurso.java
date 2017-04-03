0095b4fca416deacd4a196c55bc95bf092cd0e62
Fred William Torno Junior <fredwilliam@gmail.com>
2017-04-03 00:27:07 -0300

    ..

--- SpringBootRestFuncionarios/src/main/java/br/com/fwtj/springbootrestfuncionarios/recursos/FuncionariosRecurso.java
+++ SpringBootRestFuncionarios/src/main/java/br/com/fwtj/springbootrestfuncionarios/recursos/FuncionariosRecurso.java
@@ -38,14 +38,14 @@ public class FuncionariosRecurso {
 
     @Autowired
     FuncionarioServico funcionarioServico;
-    
-    @RequestMapping(method = RequestMethod.GET, 
-            produces = 
-                    {
-                        MediaType.APPLICATION_JSON_VALUE,
-                        MediaType.APPLICATION_XML_VALUE,
-                        "application/x-yaml"
-                    })
+
+    @RequestMapping(method = RequestMethod.GET,
+            produces
+            = {
+                MediaType.APPLICATION_JSON_VALUE,
+                MediaType.APPLICATION_XML_VALUE,
+                "application/x-yaml"
+            })
     public ResponseEntity<List<Funcionario>> listar() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         System.out.println("Usuario Logado : " + auth.getName());
@@ -59,12 +59,24 @@ public class FuncionariosRecurso {
         return ResponseEntity.created(uri).header("Resposta", funcionario.getRespostaAPI()).body(funcionario);
     }
 
+    @RequestMapping(value = "/{idFuncionario}/faltas", method = RequestMethod.POST)
+    public ResponseEntity<Falta> salvarFalta(@PathVariable Long idFuncionario, @Valid @RequestBody Falta falta) {
+        falta = funcionarioServico.salvarFalta(idFuncionario, falta);
+
+        Map<String, String> mapaUri = new HashMap<String, String>();
+        mapaUri.put("idFalta", falta.getId().toString());
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{idFalta}").buildAndExpand(mapaUri).toUri();
+
+        return ResponseEntity.created(uri).header("Resposta", falta.getRespostaAPI()).body(falta);
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> buscar(@PathVariable Long id) {
         Funcionario funcionario = funcionarioServico.buscar(id);
-        
+
         CacheControl cacheControl = CacheControl.maxAge(60, TimeUnit.SECONDS);
-        
+
         return ResponseEntity.ok().cacheControl(cacheControl).body(funcionario);
     }
 
@@ -75,38 +87,26 @@ public class FuncionariosRecurso {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Void> atualizar(@PathVariable Long id,@Valid @RequestBody Funcionario funcionario) {
+    public ResponseEntity<Void> atualizar(@PathVariable Long id, @Valid @RequestBody Funcionario funcionario) {
         funcionario.setId(id);
         funcionario = funcionarioServico.atualizar(funcionario);
         return ResponseEntity.accepted().header("Resposta", funcionario.getRespostaAPI()).build();
     }
-    
+
     @RequestMapping(value = "/{idFuncionario}/faltas", method = RequestMethod.GET)
-    public ResponseEntity<List<Falta>> listarFalta(@PathVariable Long idFuncionario){
+    public ResponseEntity<List<Falta>> listarFalta(@PathVariable Long idFuncionario) {
         List<Falta> faltas = funcionarioServico.faltas(idFuncionario);
         return ResponseEntity.ok().body(faltas);
     }
-    
-    @RequestMapping(value = "/{idFuncionario}/faltas", method = RequestMethod.POST)
-    public ResponseEntity<Void> salvarFalta(@PathVariable Long idFuncionario,@Valid @RequestBody Falta falta){
-        falta = funcionarioServico.salvarFalta(idFuncionario, falta);
-        
-        Map<String, String> mapaUri = new HashMap<String, String>();
-        mapaUri.put("idFalta", falta.getId().toString());
-        
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{idFalta}").buildAndExpand(mapaUri).toUri();
-        
-        return ResponseEntity.created(uri).header("Resposta", falta.getRespostaAPI()).build();
-    }
-    
+
     @RequestMapping(value = "/{idFuncionario}/faltas/{idFalta}", method = RequestMethod.DELETE)
-    public ResponseEntity<Void> deletarFalta(@PathVariable Long idFuncionario,@PathVariable String idFalta){
+    public ResponseEntity<Void> deletarFalta(@PathVariable Long idFuncionario, @PathVariable String idFalta) {
         String resposta = funcionarioServico.deletarFalta(idFuncionario, idFalta);
         return ResponseEntity.accepted().header("Resposta", resposta).build();
     }
-    
+
     @RequestMapping(value = "/{idFuncionario}/faltas/{idFalta}", method = RequestMethod.PUT)
-    public ResponseEntity<Void> atualizarFalta(@PathVariable Long idFuncionario,@PathVariable Long idFalta,@Valid @RequestBody Falta falta){
+    public ResponseEntity<Void> atualizarFalta(@PathVariable Long idFuncionario, @PathVariable Long idFalta, @Valid @RequestBody Falta falta) {
         falta = funcionarioServico.atualizarFalta(idFuncionario, idFalta, falta);
         return ResponseEntity.accepted().header("Resposta", falta.getRespostaAPI()).build();
     }
